670e2a135a8e9a09a7fedda0acda7b30e7f939c6
Komaru-dude <honkhonkovic842@gmail.com>
2025-06-20 00:00:50 +0700

    По просьбе @aNillerx

--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -6,4 +6,4 @@ start_router = Router()
 
 @start_router.message(Command("start"))
 async def cmd_start(message: Message):
-    await message.reply("Добро пожаловать В Panchan VPN, здесь вы может приобрести VPN для ваших нужд.")
+    await message.reply("Добро пожаловать В Panchan VPN, здесь вы можете получить ключ для VPN. Чтобы приобрести ключ нужно написать @Panchan3 или @ToboKun. /request чтобы получить ключ")
