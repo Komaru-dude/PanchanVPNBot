ee598111a83ca448e54bc348f5c24826977aa6b8
Komaru-dude <honkhonkovic842@gmail.com>
2025-07-03 22:19:36 +0700

    Не трогаем профили

--- bot/__main__.py
+++ bot/__main__.py
@@ -17,18 +17,6 @@ token = str(os.getenv("BOT_API_TOKEN"))
 bot = Bot(token)
 dp = Dispatcher()
 
-async def init_profiles(api: Api):
-    if not await api.check_user_template_exists("vless_free"):
-        logging.info("Бесплатный профиль не существует, создаём")
-        await api.add_user_template(name="vless_free", data_limit="2147483648", inbounds={"vless": ["VLESS TCP REALITY"]})
-    else:
-        logging.info("Бесплатный профиль существует")
-    if not await api.check_user_template_exists("vless_30"):
-        logging.info("Платный 30 дневный профиль не существует, создаём")
-        await api.add_user_template(name="vless_30", expire_duration=2592000, inbounds={"vless": ["VLESS TCP REALITY"]})
-    else:
-        logging.info("Платный 30 дневный профиль существует")
-
 async def main():
     api = Api()
     await api.init()
