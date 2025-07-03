abf53ab8b0d1e47e007c3028413e57105a6a1067
Komaru-dude <honkhonkovic842@gmail.com>
2025-07-03 22:30:11 +0700

    Правильно обрабатываем колбэки

--- bot/middlewares/only_private.py
+++ bot/middlewares/only_private.py
@@ -1,18 +1,16 @@
 from typing import Callable, Dict, Any, Awaitable
 from aiogram import BaseMiddleware
-from aiogram.types import TelegramObject
+from aiogram.types import TelegramObject, Message
 
 
 class OnlyPrivate(BaseMiddleware):
-    def __init__(self):
-        super().__init__()
-
     async def __call__(
         self,
         handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
         event: TelegramObject,
         data: Dict[str, Any],
     ) -> Any:
-        if event.chat.type in ("group", "supergroup", "channel"):
-            return None
-        return await handler(event, data)
+        if isinstance(event, Message):
+            if event.chat.type in ("group", "supergroup", "channel"):
+                return None
+            return await handler(event, data)
