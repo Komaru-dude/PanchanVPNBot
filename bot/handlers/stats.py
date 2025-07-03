4d4b552de8e0b97011e05832e9b8ae615ef84835
Komaru-dude <honkhonkovic842@gmail.com>
2025-07-03 22:27:34 +0700

    Чистим мусор

--- bot/handlers/stats.py
+++ bot/handlers/stats.py
@@ -5,11 +5,10 @@ from aiogram.enums import ParseMode
 
 from bot.utils.api import Api
 from bot.filters.admin_command import AdminCmdFilter
-from bot.filters.chat_type import ChatTypeFilter
 
 stats_router = Router()
 
-@stats_router.message(Command("stats"), AdminCmdFilter(), ChatTypeFilter("private"))
+@stats_router.message(Command("stats"), AdminCmdFilter())
 async def cmd_stats(message: Message, api: Api):
     data = await api.get_stats()
 
